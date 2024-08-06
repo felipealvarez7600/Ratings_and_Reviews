837de6be34966605e9d2fc8aa68b5b9888369385
49478 - Felipe Alvarez <113711798+felipealvarez7600@users.noreply.github.com>
2024-08-06 16:39:06 +0100

    - Minor fix on a mapper

--- src/main/kotlin/project/repositories/jdbi/mappers/UserMapper.kt
+++ src/main/kotlin/project/repositories/jdbi/mappers/UserMapper.kt
@@ -1,12 +1,12 @@
 package project.repositories.jdbi.mappers
 
+import kotlinx.datetime.Instant
 import org.jdbi.v3.core.mapper.ColumnMapper
 import org.jdbi.v3.core.statement.StatementContext
 import project.domain.users.Password
 import project.domain.users.User
 import java.sql.ResultSet
 import java.sql.SQLException
-import java.time.Instant
 import kotlin.jvm.Throws
 
 class UserMapper : ColumnMapper<User> {
@@ -18,7 +18,7 @@ class UserMapper : ColumnMapper<User> {
             email = r.getString("email"),
             password = Password(r.getString("password")),
             description = r.getString("description"),
-            createdAt = Instant.ofEpochMilli(r.getLong("created_at"))
+            createdAt = Instant.fromEpochSeconds(r.getLong("created_at"))
         ) else null
     }
 }
